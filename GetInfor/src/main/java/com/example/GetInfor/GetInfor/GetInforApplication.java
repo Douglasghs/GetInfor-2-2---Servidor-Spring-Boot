5dcb9a54bd902f3ad33f1f458b8d914cbb0f96ec
douglas ruan de araujo fernando <67982779+Douglasghs@users.noreply.github.com>
2021-09-28 17:21:26 -0300

    Criação do projeto Spring Boot

---
+++ GetInfor/src/main/java/com/example/GetInfor/GetInfor/GetInforApplication.java
@@ -0,0 +1,13 @@
+package com.example.GetInfor.GetInfor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetInforApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetInforApplication.class, args);
+	}
+
+}
---
+++ GetInfor/src/test/java/com/example/GetInfor/GetInfor/GetInforApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GetInfor.GetInfor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetInforApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

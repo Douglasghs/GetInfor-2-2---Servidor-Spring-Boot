b3e4024cfd397c794459d8d4253daed5a7ced09b
douglas ruan de araujo fernando <67982779+Douglasghs@users.noreply.github.com>
2021-10-16 09:47:29 -0300

    Terminando O serviço Spring Boot
    Serviço windows e servidor estão em total sincronia!!
    
    Projeto do servidor terminado

--- GetInfor/src/main/java/com/example/GetInfor/GetInfor/Controller/Controller_Rotas.java
+++ GetInfor/src/main/java/com/example/GetInfor/GetInfor/Controller/Controller_Rotas.java
@@ -1,6 +1,9 @@
 package com.example.GetInfor.GetInfor.Controller;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,13 +18,21 @@ import com.google.gson.Gson;
 public class Controller_Rotas {
 
 	ArrayList<Model_user> Lista = new ArrayList<>();
+	
+	
+	@RequestMapping(value = "/Valores", method = RequestMethod.GET)
+	public List<Model_user> GetList(){
+		return Lista;
+	}
+	
 
 	@RequestMapping(value = "/Cadastro", method = RequestMethod.POST)
-	public ResponseEntity AddValues(@RequestBody Object info) {
+	public ResponseEntity AddValues(@RequestBody String info, HttpServletResponse res) {
 		
 		try {
+			
 			Gson gson = new Gson();
-			Model_user objModel = gson.fromJson(info.toString(), Model_user.class);
+			Model_user objModel = gson.fromJson(info, Model_user.class);
 			
 			String IPV4 = objModel.getIPV4();
 			String IPV6 = objModel.getIPV6();
@@ -29,15 +40,18 @@ public class Controller_Rotas {
 			String HostName = objModel.getHostName();
 		    String Memory = objModel.getMemoryRAM();
 			
-			if((IPV4.equalsIgnoreCase("")) && (IPV6.equalsIgnoreCase("")) && (UserName.equalsIgnoreCase("")) && (HostName.equalsIgnoreCase("")) && (Memory.equalsIgnoreCase(""))) {
+		    
+			if((IPV4 != "") && (IPV6 != "") && (UserName != "") && (HostName != "") && (Memory != "")) {
 				Lista.add(new Model_user(IPV4, IPV6, UserName, HostName, Memory));
-			}else {
-				// retornar que a página nãio tá com os parametros corretos
+				
+				//Lista.add(new Model_user("194.168", "d03a:ac7c:5ef2:2790%18", "Douglas", "", ""));
+				//Lista.add(new Model_user("194.168", "d03a:ac7c:5ef2:2790%19", "Rian", "", ""));
+				
+				System.out.println("Add Sucessy");
 			}
 			
-			System.out.println(info.toString());
-		 
 			return ResponseEntity.ok(info);
+			
 		} catch (Exception e) {
 			return ResponseEntity.badRequest().build();
 
@@ -46,11 +60,36 @@ public class Controller_Rotas {
 	}
 
 	@RequestMapping(value = "/Delete", method = RequestMethod.PUT)
-	public ResponseEntity DeleteValues(@RequestBody Object info) {
+	public ResponseEntity DeleteValues(@RequestBody String info) {
 
 		try {
-			System.out.println("Delete ----------------------");
-			System.out.println(info);
+		
+			Gson gson = new Gson();
+			Model_user objModel = gson.fromJson(info, Model_user.class);
+			
+			String IPV4 = objModel.getIPV4();
+			String IPV6 = objModel.getIPV6();
+			String UserName = objModel.getUserName();
+			String HostName = objModel.getHostName();
+		    String Memory = objModel.getMemoryRAM();
+			
+		    
+			if((IPV4 != "") && (IPV6 != "") && (UserName != "") && (HostName != "") && (Memory != "")) {
+				for(int i = 0; i < Lista.size(); i++) {
+					Model_user lista = Lista.get(i);
+					
+					boolean IPV4Boll = lista.getIPV4().equals(IPV4);
+					boolean IPV6Boll = lista.getIPV6().equals(IPV6);
+					boolean UserNameBoll = lista.getUserName().equals(UserName);
+					boolean HostNameBoll = lista.getHostName().equals(HostName);
+					boolean MemoryBoll = lista.getMemoryRAM().equals(Memory);
+					
+					if(IPV4Boll && IPV6Boll && UserNameBoll && HostNameBoll && MemoryBoll) {
+						Lista.remove(lista);
+						break;
+					}
+				}
+			}
 
 			return ResponseEntity.ok(info);
 		} catch (Exception e) {
